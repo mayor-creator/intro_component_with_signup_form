860a80461d488ceae3c1de4310977d7b62fa4e7d
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2023-10-23 07:11:44 -0400

    create separate function for each input fields

--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,64 @@
-//console.log("Hello Word");
 let firstName = document.getElementById("first_name").value;
 let lastName = document.getElementById("last_name").value;
-let email = document.getElementById("email_address").value;
+let emailAddress = document.getElementById("email_address").value;
 let password = document.getElementById("password").value;
 let errorMessage = document.getElementsByClassName("error_message");
 let btn = document.getElementsByClassName("button")[0];
 
-btn.addEventListener("click", function () {
-	errorMessage[0].style.display = "block";
-	errorMessage[1].style.display = "block";
-	errorMessage[2].style.display = "block";
-	errorMessage[3].style.display = "block";
-});
+const validateFirstName = () => {
+	if (firstName) {
+		document.getElementById("first_name").style.border =
+			"2px solid  hsl(154, 59%, 51%)";
+		errorMessage[0].style.display = "none";
+	} else {
+		document.getElementById("first_name").style.border =
+			"2px solid hsl(0, 100%, 74%)";
+		errorMessage[0].style.display = "block";
+	}
+};
+
+const validateLastName = () => {
+	if (lastName) {
+		document.getElementById("last_name").style.border =
+			"2px solid  hsl(154, 59%, 51%)";
+		errorMessage[1].style.display = "none";
+	} else {
+		document.getElementById("last_name").style.border =
+			"2px solid hsl(0, 100%, 74%)";
+		errorMessage[1].style.display = "block";
+	}
+};
+
+const validateEmailAddress = () => {
+	const EMAILPATTERN = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/g;
+	if (EMAILPATTERN.test(emailAddress)) {
+		document.getElementById("email_address").style.border =
+			"2px solid  hsl(154, 59%, 51%)";
+		errorMessage[2].style.display = "none";
+	} else {
+		document.getElementById("email_address").style.border =
+			"2px solid hsl(0, 100%, 74%)";
+		errorMessage[2].style.display = "block";
+	}
+};
+
+const validatePassword = () => {
+	if (password) {
+		document.getElementById("email_address").style.border =
+			"2px solid  hsl(154, 59%, 51%)";
+		errorMessage[3].style.display = "block";
+	} else {
+		document.getElementById("password").style.border =
+			"2px solid hsl(0, 100%, 74%)";
+		errorMessage[3].style.display = "block";
+	}
+};
+
+function formValidation() {
+	validateFirstName();
+	validateLastName();
+	validateEmailAddress();
+	validatePassword();
+}
+
+btn.addEventListener("click", formValidation);
